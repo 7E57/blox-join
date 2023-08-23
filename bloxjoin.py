b00c025a80367c25fb1eb8c18e6d2b0695b30c07
7E57 <93739784+7E57@users.noreply.github.com>
2023-08-23 10:06:45 -0400

    Update bloxjoin.py

--- bloxjoin.py
+++ bloxjoin.py
@@ -10,10 +10,6 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user.name}")
-
 def generate_url(game_id, private_code):
     query_params = {'game_id': str(game_id)}
     if private_code:
@@ -22,46 +18,49 @@ def generate_url(game_id, private_code):
     unique_url = f'http://blox-join.x10.mx/redirect.html?{query_string}'
     return unique_url
 
-@bot.command()
-async def public(ctx, game_id: int):
-    game_url = generate_url(game_id, None)
-    ugc_url = "http://blox-join.x10.mx/waiting.html"
-    embed = discord.Embed(title="Open Public Roblox Game", description=f"Click the link below to open the public Roblox game:\n[Join Game]({game_url}) | [Available UGCs]({ugc_url})", color=discord.Color.green())
-    await ctx.send(embed=embed)
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name}")
 
-@bot.command()
-async def private(ctx, game_id: int, private_code: str):
-    game_url = generate_url(game_id, private_code)
-    ugc_url = "http://blox-join.x10.mx/waiting.html"
-    embed = discord.Embed(title="Open Private Roblox Game", description=f"Click the link below to open the private Roblox game:\n[Join Game]({game_url}) | [Available UGCs]({ugc_url})", color=discord.Color.blue())
-    await ctx.send(embed=embed)
-
-@bot.command()
-async def url(ctx, *, url: str):
-    parsed_url = urllib.parse.urlparse(url)
-    
-    if "roblox.com" not in parsed_url.netloc:
-        await ctx.send("Invalid Roblox URL.")
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
         return
-    
-    path_parts = parsed_url.path.split("/")
-    game_id = path_parts[2]
-    query_params = urllib.parse.parse_qs(parsed_url.query)
-    private_code = query_params.get("privateServerLinkCode")
-    
-    if private_code:
-        private_code = private_code[0]
-    if game_id:
-        game_url = generate_url(game_id, private_code)
-        ugc_url = "http://blox-join.x10.mx/waiting.html"
-        embed = discord.Embed(title="Open Roblox Game from URL", description=f"Click the link below to open the Roblox game from the URL:\n[Join Game]({game_url}) | [Available UGCs]({ugc_url})", color=discord.Color.gold())
-        await ctx.send(embed=embed)
-    else:
-        await ctx.send("Unable to extract game ID and private code from the URL.")
 
-@bot.command()
-async def ping(ctx):
-    latency = bot.latency * 1000  # Convert to milliseconds
-    await ctx.send(f"Pong! Latency: {latency:.2f} ms")
+    urls = extract_urls(message.content)
+    for url in urls:
+        parsed_url = urllib.parse.urlparse(url)
+        if "roblox.com" in parsed_url.netloc:
+            game_id, private_code = extract_game_id_and_private_code(parsed_url)
+            if game_id is not None:
+                game_url = generate_url(game_id, private_code)
+                ugc_url = "http://blox-join.x10.mx/waiting.html"
+                git_url = "https://github.com/7E57/blox-join/blob/main/bloxjoin.py"
+                embed = discord.Embed(title="Open Roblox Game from URL", description=f"Click the link below to open the Roblox game from the URL:\n[Join Game]({game_url}) | [Available UGCs]({ugc_url}) | [GitHub]({git_url})", color=discord.Color.gold())
+                await message.channel.send(embed=embed)
+            else:
+                await message.channel.send("Unable to extract game ID and private code from the URL.")
+    
+    await bot.process_commands(message)
+
+def extract_urls(text):
+    return [word for word in text.split() if word.startswith(("http://", "https://"))]
+
+def extract_game_id_and_private_code(url):
+    path_parts = url.path.split("/")
+    query_params = urllib.parse.parse_qs(url.query)
+    game_id = None
+    private_code = None
+
+    if path_parts:
+        for part in path_parts:
+            if part.isdigit():
+                game_id = int(part)
+                break
+
+    if "privateServerLinkCode" in query_params:
+        private_code = query_params["privateServerLinkCode"][0]
+
+    return game_id, private_code
 
 bot.run(TOKEN)
