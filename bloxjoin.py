2d93b8bea3ea600bf2f10f72738c3154f3c7fa1e
7E57 <93739784+7E57@users.noreply.github.com>
2023-09-01 01:12:06 -0400

    Update bloxjoin.py
    No longer responds to just any link from roblox.com (Sorry for the annoyance)

--- bloxjoin.py
+++ bloxjoin.py
@@ -2,13 +2,12 @@ import discord
 from discord.ext import commands
 import urllib.parse
 
-TOKEN = 'ADD_BOT_TOKEN'
-PREFIX = '!'
+TOKEN = 'BOT TOKEN HERE'
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix=PREFIX, intents=intents)
+bot = commands.Bot(command_prefix='', intents=intents)
 
 def generate_url(game_id, private_code):
     query_params = {'game_id': str(game_id)}
@@ -27,10 +26,10 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    urls = extract_urls(message.content)
-    for url in urls:
-        parsed_url = urllib.parse.urlparse(url)
-        if "roblox.com" in parsed_url.netloc:
+    if any(url.startswith(("http://", "https://")) and "roblox.com/games/" in url for url in message.content.split()):
+        urls = extract_urls(message.content)
+        for url in urls:
+            parsed_url = urllib.parse.urlparse(url)
             game_id, private_code = extract_game_id_and_private_code(parsed_url)
             if game_id is not None:
                 game_url = generate_url(game_id, private_code)
@@ -40,8 +39,7 @@ async def on_message(message):
                 await message.channel.send(embed=embed)
             else:
                 await message.channel.send("Unable to extract game ID and private code from the URL.")
-    
-    await bot.process_commands(message)
+    return
 
 def extract_urls(text):
     return [word for word in text.split() if word.startswith(("http://", "https://"))]
@@ -52,7 +50,7 @@ def extract_game_id_and_private_code(url):
     game_id = None
     private_code = None
 
-    if path_parts:
+    if path_parts and "roblox.com" in url.netloc and "games" in path_parts:
         for part in path_parts:
             if part.isdigit():
                 game_id = int(part)
